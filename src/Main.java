422cafd38ff86cd4ce8d561dda141f564ef1431d
undergamer3328 <kotruslan@outlook.com>
2026-06-22 22:56:13 +0300

    just making app more optimized

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.HashMap;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
